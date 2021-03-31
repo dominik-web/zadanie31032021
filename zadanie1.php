766a6dfb566a7a7e9f9da42c1f0dcea6a5c1cfaf
deo <deoka164@gmail.com>
2021-03-31 13:22:21 +0200

    koniec

--- zadanie1.php
+++ zadanie1.php
@@ -2,3 +2,6 @@
 $mysqli = new mysqli("localhost","my_user","my_password","my_db");
 if ($mysqli -> connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+?>
