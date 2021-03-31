1cdc52800b2145689ac109e37cb5129f1412de94
deo <deoka164@gmail.com>
2021-03-31 13:29:27 +0200

    poczotek

---
+++ zadanie3.php
@@ -0,0 +1,6 @@
+<?php
+$mysql_host = 'localhost'; //lub jakiś adres: np sql.nazwa_bazy.nazwa.pl
+$port = '3307'; //domyślnie jest to port 3306
+$username = 'login';
+$password = 'hasło';
+$database = 'nazwa_bazy'; //'produkty'
