861b8cc6f168d39798be6edd203a3414da1b2f0f
deo <deoka164@gmail.com>
2021-03-31 13:27:42 +0200

    koncowka

--- zadanie2.php
+++ zadanie2.php
@@ -3,3 +3,5 @@ $servername = "localhost";
 $username = "username";
 $password = "password";
 $conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
